15167a049e52c0bef21e6e444970e3c288dc195d
Abderrahmane <abderrahmanelahmidi94@gmail.com>
2023-02-13 09:45:12 +0100

    This React Bank-Modern Website

---
+++ modern/src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Button = ({ styles }) => {
+  return (
+   <button
+   type='button'
+   className={`py-4 px-6 rounded-[10px] bg-blue-gradient font-poppins
+    font-medium text-[18px]
+   text-primary outline-none
+   ${styles}`}
+   >
+   Get Stared
+   </button>
+  )
+}
+
+export default Button
